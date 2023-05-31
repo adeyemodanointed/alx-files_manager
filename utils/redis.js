c686532a7e57dedac374d43cee1c487627fb016a
Peter Adeyemo <adeyemo@tm30.net>
2023-05-31 15:54:04 +0100

    Start up

---
+++ utils/redis.js
@@ -0,0 +1,53 @@
+import { createClient } from "redis";
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on("error", (err) => {
+      console.log(err);
+    });
+  }
+
+  isAlive() {
+    // return this.client.ping((err, response) => {
+    //   console.log(response);
+    //   if (response === "PONG") {
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // });
+    return this.client.connected;
+  }
+
+  async get(key) {
+    this.client.get(key, (err, result) => {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      return result;
+    });
+  }
+
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+  }
+
+  async del(key) {
+    this.client.del(key, (err, result) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
